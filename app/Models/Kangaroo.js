e2a40f8b71b3ee60757b197a7290e4b52b282192
CaitlinCodes <caiit@me.com>
2018-08-06 15:25:34 -0600

    kangaroo - quail

---
+++ app/Models/Kangaroo.js
@@ -0,0 +1,19 @@
+class Kangaroo {
+  //WHAT ARE THE FORM FIELDS?
+  constructor(
+    name,
+    gender,
+    age,
+    weight,
+    color,
+  ) {
+    this.id = Math.random()
+    this.name = name
+    this.gender = gender
+    this.age = age
+    this.weight = weight
+    this.color = color
+  }
+}
+
+export default Kangaroo
