ea14126d3543001005534c2f3cd5b0a52b544e0e
CaitlinCodes <caiit@me.com>
2018-08-06 15:30:44 -0600

    rhino-zebra

---
+++ app/Models/Snake.js
@@ -0,0 +1,21 @@
+class Snake {
+  //WHAT ARE THE FORM FIELDS?
+  constructor(
+    name,
+    gender,
+    age,
+    weight,
+    length,
+    color,
+  ) {
+    this.id = Math.random()
+    this.name = name
+    this.gender = gender
+    this.age = age
+    this.weight = weight
+    this.length = length
+    this.color = color
+  }
+}
+
+export default Snake
---
+++ app/Models/Vulture.js
@@ -0,0 +1,21 @@
+class Vulture {
+  //WHAT ARE THE FORM FIELDS?
+  constructor(
+    name,
+    gender,
+    age,
+    weight,
+    length,
+    color,
+  ) {
+    this.id = Math.random()
+    this.name = name
+    this.gender = gender
+    this.age = age
+    this.weight = weight
+    this.length = length
+    this.color = color
+  }
+}
+
+export default Vulture
