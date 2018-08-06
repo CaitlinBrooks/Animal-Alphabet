eb089527afe6f6bf25568fd4768251ac1b1d4270
CaitlinCodes <caiit@me.com>
2018-08-06 14:09:06 -0600

    alligator

---
+++ app/Models/Alligator.js
@@ -0,0 +1,21 @@
+
+
+class Alligator {
+  //WHAT ARE THE FORM FIELDS?
+  constructor(
+    name,
+    gender,
+    age,
+    length,
+    weight,
+  ) {
+    this.id = Math.random()
+    this.name = name
+    this.gender = gender
+    this.age = age
+    this.length = length
+    this.weight = weight
+  }
+}
+
+export default Alligator
