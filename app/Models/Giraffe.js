c275a302068415756266a874a13135ba9278c845
CaitlinCodes <caiit@me.com>
2018-08-06 15:23:20 -0600

    giraffe - jellyfish

---
+++ app/Models/Giraffe.js
@@ -0,0 +1,17 @@
+class Giraffe {
+  //WHAT ARE THE FORM FIELDS?
+  constructor(
+    name,
+    gender,
+    age,
+    weight,
+  ) {
+    this.id = Math.random()
+    this.name = name
+    this.gender = gender
+    this.age = age
+    this.weight = weight
+  }
+}
+
+export default Giraffe
---
+++ app/Models/Hippo.js
@@ -0,0 +1,17 @@
+class Hippo {
+  //WHAT ARE THE FORM FIELDS?
+  constructor(
+    name,
+    gender,
+    age,
+    weight,
+  ) {
+    this.id = Math.random()
+    this.name = name
+    this.gender = gender
+    this.age = age
+    this.weight = weight
+  }
+}
+
+export default Hippo
