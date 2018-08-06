802e82d217b0d22c9c406e1cff46dc27c9fb6b62
CaitlinCodes <caiit@me.com>
2018-08-06 14:41:04 -0600

    elephant and fox

---
+++ app/Models/Fox.js
@@ -0,0 +1,23 @@
+class Fox {
+  //WHAT ARE THE FORM FIELDS?
+  constructor(
+    name,
+    gender,
+    age,
+    type,
+    weight,
+    trunkLength,
+    color,
+  ) {
+    this.id = Math.random()
+    this.name = name
+    this.gender = gender
+    this.age = age
+    this.type = type
+    this.weight = weight
+    this.trunkLength = trunkLength
+    this.color = color
+  }
+}
+
+export default Fox
